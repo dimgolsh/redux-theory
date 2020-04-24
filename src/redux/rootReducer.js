0aaf22a2f8f88b790bcfaa71a45f94e6a6f584a6
dimon <dimagol64@gmail.com>
2020-04-24 12:08:50 +0300

    redux init

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+  counter: 100,
+};
+
+export default function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "ADD":
+      return {
+        counter: state.counter + 1,
+      };
+
+    case "SUB":
+      return {
+        counter: state.counter - 1,
+      };
+
+    case "NUM":
+      return {
+        counter: state.counter - 1,
+      };
+
+    default:
+      return state;
+  }
+
+  return state;
+}
